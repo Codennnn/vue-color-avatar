666162cbd5f1abe801ae81190acdcaecb57985e5
LeoKu <leokudev@gmail.com>
2024-01-11 22:26:42 +0800

    Add Zeabur Badge

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -19,6 +19,15 @@
     <div class="locale" @click="switchLocale">
       {{ locale === Locale.EN ? '简体中文' : 'English' }}
     </div>
+
+    <div class="divider">|</div>
+
+    <a
+      href="https://zeabur.com?referralCode=Codennnn&utm_source=Codennnn"
+      target="_blank"
+    >
+      <img src="https://zeabur.com/deployed-on-zeabur-dark.svg" />
+    </a>
   </footer>
 </template>
 
